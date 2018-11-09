d422d098734fcdf5c8071d9b737d664245603d52
enjoy <59070049@it.kmitl.ac.th>
2018-11-09 13:44:03 +0700

    update login fragment

--- FindBuffet/app/src/main/java/com/nawinc27/mac/findbuffet/LoginFragment.java
+++ FindBuffet/app/src/main/java/com/nawinc27/mac/findbuffet/LoginFragment.java
@@ -9,8 +9,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.nawinc27.mac.findbuffet.Main_menu.MainPageFragment;
 
 public class LoginFragment extends Fragment {
+
+    private 
 
     private EditText email_input ;
     private EditText password_input;
@@ -21,8 +26,10 @@ public class LoginFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        email_input = (EditText) getActivity().findViewById(R.id.email_input_register);
-        password_input = (EditText) getActivity().findViewById(R.id.password_input_register);
+
+        email_input = (EditText) getActivity().findViewById(R.id.login_email_input_register);
+        password_input = (EditText) getActivity().findViewById(R.id.login_password_input_register);
+
         login_btn = (TextView)getActivity().findViewById(R.id.login_btn_login);
         register_btn = (TextView) getActivity().findViewById(R.id.register_btn_login);
 
@@ -30,16 +37,39 @@ public class LoginFragment extends Fragment {
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.main_view, new MainPageFragment())
+                        .addToBackStack(null)
+                        .commit();
             }
         });
 
         register_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.main_view, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
 
             }
         });
+    }
+
+    public void login(){
+
+
+        String emailSrr = email_input.getText().toString();
+        String passwordStr = password_input.getText().toString();
+        if(emailSrr.isEmpty() || passwordStr.isEmpty()){
+            Toast.makeText(getActivity(),"Login failed", Toast.LENGTH_SHORT).show();
+        }else{
+
+        }
+
+
     }
 
     @Nullable
