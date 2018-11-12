13d010f4c440d214b52a3c004f960c182846ca60
nawinest <nawinc.work@gmail.com>
2018-11-12 16:08:09 +0700

    add Search filter

---
+++ FindBuffet/app/src/main/java/com/nawinc27/mac/findbuffet/Buffet_List/BuffetAdapter.java
@@ -0,0 +1,108 @@
+package com.nawinc27.mac.findbuffet.Buffet_List;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.TextView;
+
+import com.nawinc27.mac.findbuffet.Model.Buffet;
+import com.nawinc27.mac.findbuffet.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BuffetAdapter extends BaseAdapter implements Filterable {
+
+    private List<Buffet> buffets;
+    private List<Buffet> filterList;
+    private Context context;
+    CustomFilter filter;
+
+    public BuffetAdapter(Context c, ArrayList<Buffet> b){
+        this.context = c;
+        this.buffets = b;
+        this.filterList = b;
+    }
+
+
+    @Override
+    public int getCount() {
+        return buffets.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return buffets.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return buffets.indexOf(position);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View buffetItem = LayoutInflater.from(context).inflate(R.layout.buffetlist_item, parent, false);
+        TextView name_th = buffetItem.findViewById(R.id.name_th);
+        TextView name_en = buffetItem.findViewById(R.id.name_en);
+
+        name_th.setText(buffets.get(position).getName_th());
+        Log.d("Buffet" , buffets.get(position).getName_th() + " from adapter");
+        name_en.setText(buffets.get(position).getName_en());
+        return buffetItem;
+    }
+
+    @Override
+    public Filter getFilter() {
+        if(filter == null){
+            filter = new CustomFilter();
+        }
+        return filter;
+    }
+
+    class CustomFilter extends Filter{
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults results = new FilterResults();
+            if (constraint != null && constraint.length() > 0){
+                constraint = constraint.toString().toUpperCase();
+                ArrayList<Buffet> filters = new ArrayList<Buffet>();
+
+                for (int i = 0 ;i < filterList.size() ; i ++){
+                    if(filterList.get(i).getName_en().contains(constraint)){
+                        Buffet b = new Buffet(filterList.get(i).getName_th(),
+                                filterList.get(i).getName_en(),
+                                filterList.get(i).getDescrition(),
+                                filterList.get(i).getContact(),
+                                filterList.get(i).getTime(),
+                                filterList.get(i).getLat(),
+                                filterList.get(i).getLng(),
+                                filterList.get(i).getType_buffet());
+                        filters.add(b);
+                    }
+                }
+                results.count = filters.size();
+                results.values = filters;
+
+            }else{
+                results.count = filterList.size();
+                results.values = filterList;
+            }
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            buffets = (List<Buffet>) results.values;
+            notifyDataSetChanged();
+        }
+    }
+
+}
--- FindBuffet/app/src/main/java/com/nawinc27/mac/findbuffet/Buffet_List/BuffetList_fragment.java
+++ FindBuffet/app/src/main/java/com/nawinc27/mac/findbuffet/Buffet_List/BuffetList_fragment.java
@@ -4,19 +4,20 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.TextView;
 
 import com.nawinc27.mac.findbuffet.Main_menu.MainPageFragment;
 import com.nawinc27.mac.findbuffet.Model.Buffet;
 import com.nawinc27.mac.findbuffet.R;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +27,7 @@ public class BuffetList_fragment extends Fragment {
     private GridView buffetGrid;
     private List<Buffet> buffets = new ArrayList<Buffet>();
     private String header;
+    private BuffetAdapter grid_adapter;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -39,49 +41,64 @@ public class BuffetList_fragment extends Fragment {
         }
 
 
+        buffets.add(new Buffet("ร้านหวังหมาฮั่น" , "Ma Hun" , "ร้านอาหารอายุ 100 ปี  "
+                , " 097 - 0699888" , "เปิดทุกวันจันทร์ - ศุกร์ | 09 : 00 - 24 : 00"
+                , "13.818340","100.588078",1));
+        buffets.add(new Buffet("ร้านหวังหมาฮั่น" , "ZAZAZA" , "ร้านอาหารอายุ 100 ปี  "
+                , " 097 - 0699888" , "เปิดทุกวันจันทร์ - ศุกร์ | 09 : 00 - 24 : 00"
+                , "13.818340","100.588078",1));
+        buffets.add(new Buffet("ร้านหวังหมาฮั่น" , "Ma Hun" , "ร้านอาหารอายุ 100 ปี  "
+                , " 097 - 0699888" , "เปิดทุกวันจันทร์ - ศุกร์ | 09 : 00 - 24 : 00"
+                , "13.818340","100.588078",1));
+        buffets.add(new Buffet("ร้านบุเฟต์ทะเลเผาเนื้อย่าง สาขาราชดำเนิน" , "Ma Hun" , "ร้านอาหารอายุ 100 ปี  "
+                , " 097 - 0699888" , "เปิดทุกวันจันทร์ - ศุกร์ | 09 : 00 - 24 : 00"
+                , "13.818340","100.588078",1));
+        buffets.add(new Buffet("ร้านหวังหมาฮั่น" , "Ma Hun" , "ร้านอาหารอายุ 100 ปี  "
+                , " 097 - 0699888" , "เปิดทุกวันจันทร์ - ศุกร์ | 09 : 00 - 24 : 00"
+                , "13.818340","100.588078",1));
+        buffets.add(new Buffet("ร้านบุเฟต์ทะเลเผาเนื้อย่าง สาขาราชดำเนิน" , "Zap Kua" , "ร้านอาหารอายุ 100 ปี  "
+                , " 097 - 0699888" , "เปิดทุกวันจันทร์ - ศุกร์ | 09 : 00 - 24 : 00"
+                , "13.818340","100.588078",1));
+
+
+        EditText filterText = getActivity().findViewById(R.id.search_bar);
+        filterText.addTextChangedListener(filterTextWatcher);
+
         buffetGrid = getActivity().findViewById(R.id.grid_buffet_list);
-        final BuffetGridAdapter grid_adapter = new BuffetGridAdapter(getActivity(), android.R.layout.list_content , buffets);
+        buffetGrid.setTextFilterEnabled(true);
+        grid_adapter = new BuffetAdapter(getActivity() , (ArrayList<Buffet>) buffets);
         buffetGrid.setAdapter(grid_adapter);
-        grid_adapter.clear();
 
-        buffets.add(new Buffet("ร้านหวังหมาฮั่น" , "Ma Hun" , "ร้านอาหารอายุ 100 ปี  "
-                , " 097 - 0699888" , "เปิดทุกวันจันทร์ - ศุกร์ | 09 : 00 - 24 : 00"
-                , "13.818340","100.588078",1));
-        buffets.add(new Buffet("ร้านหวังหมาฮั่น" , "Ma Hun" , "ร้านอาหารอายุ 100 ปี  "
-                , " 097 - 0699888" , "เปิดทุกวันจันทร์ - ศุกร์ | 09 : 00 - 24 : 00"
-                , "13.818340","100.588078",1));
-        buffets.add(new Buffet("ร้านหวังหมาฮั่น" , "Ma Hun" , "ร้านอาหารอายุ 100 ปี  "
-                , " 097 - 0699888" , "เปิดทุกวันจันทร์ - ศุกร์ | 09 : 00 - 24 : 00"
-                , "13.818340","100.588078",1));
-        buffets.add(new Buffet("ร้านหวังหมาฮั่น" , "Ma Hun" , "ร้านอาหารอายุ 100 ปี  "
-                , " 097 - 0699888" , "เปิดทุกวันจันทร์ - ศุกร์ | 09 : 00 - 24 : 00"
-                , "13.818340","100.588078",1));
-        buffets.add(new Buffet("ร้านหวังหมาฮั่น" , "Ma Hun" , "ร้านอาหารอายุ 100 ปี  "
-                , " 097 - 0699888" , "เปิดทุกวันจันทร์ - ศุกร์ | 09 : 00 - 24 : 00"
-                , "13.818340","100.588078",1));
-        buffets.add(new Buffet("ร้านหวังหมาฮั่นเปิด 24 ชั่วโมงมาแดกได้" , "Ma Hun" , "ร้านอาหารอายุ 100 ปี  "
-                , " 097 - 0699888" , "เปิดทุกวันจันทร์ - ศุกร์ | 09 : 00 - 24 : 00"
-                , "13.818340","100.588078",1));
-        buffets.add(new Buffet("นบุเฟต์ทาเนื้อย่าง สาขาราชดำเนิน" , "Ma Hun" , "ร้านอาหารอายุ 100 ปี  "
-                , " 097 - 0699888" , "เปิดทุกวันจันทร์ - ศุกร์ | 09 : 00 - 24 : 00"
-                , "13.818340","100.588078",1));
-        buffets.add(new Buffet("ร้านบุเฟต์ทะเลเผาเนื้อย่าง สาขาราชดำเนิน" , "Ma Hun" , "ร้านอาหารอายุ 100 ปี  "
-                , " 097 - 0699888" , "เปิดทุกวันจันทร์ - ศุกร์ | 09 : 00 - 24 : 00"
-                , "13.818340","100.588078",1));
-        buffets.add(new Buffet("ร้านหวังหมาฮั่น" , "Ma Hun" , "ร้านอาหารอายุ 100 ปี  "
-                , " 097 - 0699888" , "เปิดทุกวันจันทร์ - ศุกร์ | 09 : 00 - 24 : 00"
-                , "13.818340","100.588078",1));
-        buffets.add(new Buffet("ร้านบุเฟต์ทะเลเผาเนื้อย่าง สาขาราชดำเนิน" , "Ma Hun" , "ร้านอาหารอายุ 100 ปี  "
-                , " 097 - 0699888" , "เปิดทุกวันจันทร์ - ศุกร์ | 09 : 00 - 24 : 00"
-                , "13.818340","100.588078",1));
-
-
-
-        Log.d("Buffet List" , buffets.get(0).getName_th());
 
         grid_adapter.notifyDataSetChanged();
         initBackBtn();
     }
+
+    private TextWatcher filterTextWatcher = new TextWatcher() {
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count,
+                                      int after) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before,
+                                  int count) {
+            if (grid_adapter != null) {
+                grid_adapter.getFilter().filter(s);
+            } else {
+                Log.d("filter", "no filter availible");
+            }
+        }
+    };
 
     @Nullable
     @Override
